844482610845d4c52e2a9ff6b99b072a90ee0054
trusthemind <pluserwork24@gmail.com>
2023-06-26 21:07:55 +0300

    "login commit"

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,11 +4,10 @@ import { auth } from "../../firebase";
 import { NavLink } from 'react-router-dom';
 
 const Home = () => {
-    const user = auth.currentUser;
 
     return (
         <>
-            {user ? <p>Welcome Home, {user.email}</p> : <p>NO USER</p>}
+            
             <NavLink to='/sign-up'>Sign Up</NavLink>
             <br />
             <NavLink to='/log-in'>Log In</NavLink>
