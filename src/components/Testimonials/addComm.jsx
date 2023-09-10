a513e2f09c4c0a85162310a5f761ec45424ef087
Fedir Melnyk <pluserwork24@gmail.com>
2023-09-10 15:54:54 +0300

    upd

---
+++ src/components/Testimonials/addComm.jsx
@@ -0,0 +1,10 @@
+import { Card, Input, Buttom } from "antd";
+function AddTestimonial() {
+    return ( 
+        <Card>
+            <h1>Addd</h1>
+        </Card>
+     );
+}
+
+export default AddTestimonial;
